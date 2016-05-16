c94411d6a13e8fdb4cca505bff1602704c5a4916
Dodge W. Coates <dcoates91@gmail.com>
2016-05-15 20:18:55 -0400

    Add source file for leven-squash base

---
+++ levenshtein/leven-squash.py
@@ -0,0 +1,15 @@
+import logging
+from Levenshtein import
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class LevenSquash:
+    def __init__(self):
+        pass
+
+    # this exists to provide the flexibility for different LD algorithms
+    def get_distance(self):
+
+        return
