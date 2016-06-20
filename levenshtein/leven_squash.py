c95019982e1b598d59a71a0be33ae71e441fbc4c
Dodge W. Coates <dcoates91@gmail.com>
2016-06-19 23:11:22 -0400

    Add beginnings of cached LevenSquash module

--- levenshtein/leven_squash.py
+++ levenshtein/leven_squash.py
@@ -2,6 +2,7 @@ import logging
 
 from levenshtein import distance, compression
 from levenshtein.utils.filer import normalize_from_file
+from copy import deepcopy
 
 if __name__ == '__main__':
     from levenshtein.utils.log import setup_logging
@@ -11,6 +12,18 @@ logger = logging.getLogger(__name__)
 
 
 class LevenSquash:
+    # Amount by which the LD of a pair of equal length random text strings is
+    # smaller than their length. This is used to fudge the product of
+    # signature LD and compression factor to adjust the expectation.
+    # Note, this is a function of the alphabet length of the compression
+    # scheme.
+    wholeFileRatio = 0.022
+
+    # Amount by which the LD of a pair of equal size plain-generated
+    # signatures is smaller than their length. This is used to fudge product
+    # of signature LD and compression factor to adjust the expectation.
+    sigRatio = 0.030
+
     def __init__(self, compressor=None, dist_alg=None):
         # Default compression scheme. Note that this has a fairly large C,
         # so small strings will be completely annihilated, and therefore
@@ -44,7 +57,7 @@ class LevenSquash:
     def set_ld_alg(self, dist_alg):
         self._dist_alg = dist_alg
 
-    def estimate(self, str1, str2):
+    def compute_sig_dist(self, str1, str2):
         """
         Accepts two strings, returning their leven-squash signature distance.
         """
@@ -64,9 +77,51 @@ class LevenSquash:
 
         return squash_dist
 
+    def _estimate(self, str1, str2):
+        logger.info("Squashing distance between two strings...")
+
+        sig_dist = self.compute_sig_dist(str1, str2)
+
+        approx = sig_dist * self._sc.getC()
+
+        logger.info("Signature distance " +
+                    str(sig_dist) +
+                    " Scaled by compression factor " +
+                    str(self._sc.getC()) + ": " +
+                    str(approx))
+
+        return approx
+
+    def estimate(self, str1, str2):
+        """
+        Accepts two strings, and returns the approximation of their distance.
+        This is computed by squashing them, getting the distance between
+        squashes, then scaling the result by the compresson factor.
+        """
+        logger.info("Computing basic squash estimate...")
+
+        return self._estimate(str1, str2)
+
+    def estimate_corrected(self, str1, str2):
+        """
+        Accepts two strings, and returns the approximation of their distance.
+        This is derived from LevenSquash.estimate() by multiplying the result
+        by a correction factor.
+        """
+        logger.info("Computing corrected squash estimate...")
+
+        est = self._estimate(str1, str2)
+
+        # TODO: correct est by correction factor
+        est_corrected = 0 * est
+
+        logger.info("Corrected distance: " + str(est_corrected))
+
+        return est_corrected
+
     def compress(self, string):
         """
-        Use set compression scheme to compress string
+        Use set compression scheme to compress string.
         """
         return self._sc.compress(string)
 
@@ -89,3 +144,103 @@ class LevenSquash:
             raise TypeError(warning)
 
         return dist
+
+
+class LevenSquasher:
+    def __init__(self, str1, str2, ls=LevenSquash()):
+        # hmm
+        #self.log = logging.getLogger()
+
+        # strings should be stored as hashtable keys
+        # values are signatures and time to compute them
+
+        self._str1 = str1
+        self._str2 = str2
+
+        self._ls = ls
+
+        self._cache = dict()
+
+    def reset_cache(self):
+        """
+        Reset the ScoreDistance instance's cache. Useful externally if you
+        want to recalculate computation time for some cached method.
+        """
+        self._cache = dict()
+
+    def _get_value(self, fun, *args):
+        """
+        Return the cached value of the calling function + args if they've
+        already been computed. Otherwise, compute the function and return the
+        result.
+        """
+        for arg in args:
+            if arg != self._str1 and arg != self._str2:
+                self.reset_cache()
+                break
+
+        key = fun.__name__ + ''.join(args)
+
+        if key in self._cache:
+            return self._cache[key]
+
+        val = fun(*args)
+        self._cache[key] = val
+
+        return val
+
+    def get_leven_squash(self):
+        """
+        Returns a deep copy of the LevenSquash module being scored.
+        """
+        return deepcopy(self._ls)
+
+    def set_leven_squash(self, ls):
+        self.reset_cache()
+        self._ls = ls
+
+    def set_strings(self, str1=None, str2=None):
+        """
+        Sets the strings to str1 and str2. If strx is None, then it is unaffected.
+        """
+        pass
+
+    def get_strings():
+        """
+        Returns a tuple consisting of the stings being used by the squasher
+        """
+        pass
+
+    def set_str1(self, str1):
+        self.reset_cache()
+
+        self._cache[self._str1] = self._ls.compress(self._str1)
+        self._str1 = str1
+
+    def set_str2(self, str2):
+        self.reset_cache()
+
+        self._cache[self._str2] = self._ls.compress(self._str2)
+        self._str2 = str2
+
+    def get_str1(self):
+        return self._str1
+
+    def get_str2(self):
+        return self._str2
+
+    def compress(self, string):
+        # this is possibly a resource leak
+        # should return sig1 or sig2 or compute the signature, and if
+        return self._get_value(self._ls.compress, string)
+
+    def calculate(self):
+        return self._get_value(self._ls.calculate, self._str1, self._str2)
+
+    def estimate(self):
+        return self._get_value(self._ls.estimate, self._str1, self._str2)
+
+    def estimate_corrected(self):
+        return self._get_value(self._ls.estimate_corrected,
+                               self._str1,
+                               self._str2)
