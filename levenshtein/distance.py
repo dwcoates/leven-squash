b8badc2a56f9e707f087d40c6ead6bfac20e4fdb
Dodge W. Coates <dcoates91@gmail.com>
2016-06-28 21:15:00 -0400

    Add CachedLDAlgorithm.

--- levenshtein/distance.py
+++ levenshtein/distance.py
@@ -1,14 +1,28 @@
 from Levenshtein.StringMatcher import StringMatcher
 
+from levenshtein.utils.computation import CalculationCache
+
 
 class LDAlgorithm:
+
     def distance(self, str1, str2):
         raise NotImplemented()
+
+
+class CachedLDAlgorithm:
+
+    def __init__(self, ld_algorithm):
+        self._ld = ld_algorithm
+        self._cache = CalculationCache()
+
+    def distance(self, str1, str2):
+        return self._cache.produce(self._ld.distance, str1, str2)
 
 
 class AbsoluteLD(LDAlgorithm):
     """Simple wrapper class for encapsulating the
     Levenshtein.StringMatcher.distance LD algorithm."""
+
     def distance(self, str1, str2):
         sm = StringMatcher(None, str1, str2)
 
