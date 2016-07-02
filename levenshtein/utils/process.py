9d1a53560942b9bcd39933e7b024d867d123866b
Dodge W. Coates <dcoates91@gmail.com>
2016-07-02 03:55:35 -0400

    Fix typecheck in Process

--- levenshtein/utils/process.py
+++ levenshtein/utils/process.py
@@ -1,4 +1,5 @@
 from levenshtein.utils.computation import CalculationCache, ComputationManager
+import inspect
 
 
 class Process (object):
@@ -21,6 +22,7 @@ class CachedProcess (Process):
     def __init__(self, process):
         self._cache = CalculationCache()
         self._process = CachedProcess.MAKE_UNCACHED_PROCESS(process)
+        print("UNCACHED PROCESS TYPE: " + str(type(self._process)))
 
     def __call__(self, *args):
         try:
@@ -31,19 +33,19 @@ class CachedProcess (Process):
 
     @staticmethod
     def MAKE_CACHED_PROCESS(uncached_process):
-        if uncached_process.__class__.__name__ != CachedProcess.__name__:
-            return CachedProcess(uncached_process)
-        elif uncached_process.__class__.__name__ == CachedProcess.__name__:
+        if CachedProcess in inspect.getmro(uncached_process.__class__):
             return uncached_process
+        elif Process in inspect.getmro(uncached_process.__class__):
+            return CachedProcess(uncached_process)
         else:
             raise ValueError("'MAKE_CACHED_VERSION' only accepts a " +
                              "parameter of type 'Process'")
 
     @staticmethod
     def MAKE_UNCACHED_PROCESS(cached_process):
-        if cached_process.__class__.__name__ == CachedProcess.__name__:
+        if CachedProcess in inspect.getmro(cached_process.__class__):
             return cached_process._process
-        elif cached_process.__class__.__name__ != CachedProcess.__name__:
+        elif Process in inspect.getmro(cached_process.__class__):
             return cached_process
         else:
             raise ValueError("'MAKE_CACHED_VERSION' only accepts a " +
