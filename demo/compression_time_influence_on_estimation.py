6f8c16a918a010bcd790798179e0fd89591fbd79
Dodge W. Coates <dcoates91@gmail.com>
2016-07-08 17:32:27 -0400

    Clean compression time influence

--- demo/compression_time_influence_on_estimation.py
+++ demo/compression_time_influence_on_estimation.py
@@ -24,7 +24,7 @@ def demo(compressor=None):
     r2 = r(BIG_STR_LEN)
     end = time.clock()
 
-    print("Time to generate two random strings: " + str(end-start) + "\n")
+    print("Time to generate two random strings: " + str(end - start) + "\n")
 
     compression_factor = ls.get_compressor().getC()
     print("Compression factor: " + str(compression_factor) + "\n")
@@ -32,12 +32,12 @@ def demo(compressor=None):
     start = time.clock()
     sig1 = ls.compress(r1)
     end = time.clock()
-    t_sig1 = end-start
+    t_sig1 = end - start
 
     print("Time to compress random string: " + str(t_sig1))
 
     sig1_len = len(sig1)
-    expected_len = BIG_STR_LEN/compression_factor
+    expected_len = BIG_STR_LEN / compression_factor
 
     print("Compressed string length: " + str(sig1_len))
     print("Expected Compressed string length: " + str(expected_len))
@@ -48,12 +48,12 @@ def demo(compressor=None):
     start = time.clock()
     sig2 = ls.compress(r2)
     end = time.clock()
-    t_sig2 = end-start
+    t_sig2 = end - start
 
     print("Time to compress random string: " + str(t_sig2))
 
     sig2_len = len(sig2)
-    expected_len = BIG_STR_LEN/compression_factor
+    expected_len = BIG_STR_LEN / compression_factor
 
     print("Compressed string length: " + str(sig2_len))
     print("Expected Compressed string length: " + str(expected_len))
@@ -68,7 +68,7 @@ def demo(compressor=None):
     start = time.clock()
     calc = ls.calculate(r1, r2)
     end = time.clock()
-    t_calc = end-start
+    t_calc = end - start
 
     print("Calculated distance: " + str(calc))
     print("Time to calculate LD: " + str(t_calc) + "\n")
@@ -76,27 +76,29 @@ def demo(compressor=None):
     start = time.clock()
     sig_dist = ls.calculate(sig1, sig2)
     end = time.clock()
-    t_est = end-start
+    t_est = end - start
     t_est_total = t_est + t_sig_total
-    est = sig_dist*compression_factor
+    est = sig_dist * compression_factor
 
     print("Estimated distance: " + str(est))
     print("Time to estimate LD (not including compression): " + str(t_est))
     print("Time to estimate LD (total time): " + str(t_est_total) + "\n")
 
     est_err = ScoreDistance.error(est, calc)
-    print("Estimate error: " + "{:.3f}%".format(est_err*100))
+    print("Estimate error: " + "{:.3f}%".format(est_err * 100))
 
-    print("Speedup (not including compression time): " + str(int(t_calc/t_est)) +
+    print("Speedup (not including compression time): " +
+          str(int(t_calc / t_est)) +
           "x")
-    print("Speedup (including compression time): " + str(int(t_calc/t_est_total)) +
+    print("Speedup (including compression time): " +
+          str(int(t_calc / t_est_total)) +
           "x")
 
-    compression_time_factor = t_sig_total/t_est_total
+    compression_time_factor = t_sig_total / t_est_total
     print("Portion of estimation time spent on compression: " +
-          str(Decimal(compression_time_factor)*Decimal(100)) + "%")
+          str(Decimal(compression_time_factor) * Decimal(100)) + "%")
 
-    
+
 def time_compression(sc=StringCompressorBasic()):
     BIG_STR_LEN = 10000
     t_total = 0
@@ -120,10 +122,10 @@ def time_compression(sc=StringCompressorBasic()):
         t_total += t
 
     avg_len = len_sum / num_iters
-    t_avg = t_total/num_iters
+    t_avg = t_total / num_iters
 
     print("Finished.")
     print("Time to compress '" + str(num_iters) + "' strings of length '" +
-          str(BIG_STR_LEN) + "': " + str(t_total)) 
+          str(BIG_STR_LEN) + "': " + str(t_total))
     print("Average time per compression: " + str(t_avg))
     print("Average length of compressions: " + str(avg_len))
