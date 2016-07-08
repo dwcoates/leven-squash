172a6237ddd7a74d8b05af8b8c003d027510dd2f
Dodge W. Coates <dcoates91@gmail.com>
2016-07-07 22:16:37 -0400

    Add some tests to N,C range tester

--- demo/range.py
+++ demo/range.py
@@ -1,7 +1,10 @@
+import pprint
+
 from levenshtein.leven_squash import *
 from itertools import product
 from levenshtein.compression import Compressor, CRCCompression
 from levenshtein.score import ScoreDistance
+from levenshtein.distance import Absolute
 
 
 def read(fname):
@@ -9,6 +12,10 @@ def read(fname):
         data = f.read().replace('\n', '')
 
     return data
+
+distance = Absolute()
+
+pp = pprint.PrettyPrinter(indent=4)
 
 huck = read("../demo/data/adventures_of_huckleberry_finn.txt")
 sawyer = read("../demo/data/adventures_of_tom_sawyer.txt")
@@ -18,15 +25,33 @@ italian = read("../demo/data/dantes_inferno_italian.txt")
 sample_en = read("../demo/data/test1.txt")
 sample_la = read("../demo/data/test2.txt")
 
-true_huck_sawyer = 124865
+dist_huck_sawyer = 124865
+dist_sibbald_longfellow = 102076
+dist_longfellow_italian = 106560
+dist_huck_longfellow = 122229
+dist_loremIpsum_en_la = 724
+
+# print("calculating huck, sawyer")
+# dist_dist = distance(huck, sawyer)
+# print("calculating sibbald, longfellow: ")
+# dist_sibbald_longfellow = distance(sibbald, longfellow)
+# print("calculating longfellow, italian: ")
+# dist_longfellow_italian = distance(longfellow, italian)
+# print("calculating huck, longfellow")
+# dist_huck_longfellow = distance(huck, longfellow)
+# print("calculating lorem ipsum")
+# dist_loremIpsum_en_la = distance(sample_en, sample_la)
+
+C1 = 100
+C2 = 200
+N1 = 5
+N2 = 50
+STEP_C = 10
+STEP_N = 5
 
 
 def results_over_n(str1, str2, n1, n2, c, step=10):
     def LS(c, n): return LevenSquash(Compressor(CRCCompression(), C=c, N=n))
-    if n2 > n1 > 0:
-        return zip(xrange(n1, n2), [LS(n, c).estimate(str1, str2) for n in xrange(n1, n2)])
-    else:
-        raise ValueError("n1 and n2 must be positive with n2 greater than n1")
 
 
 def estimate(str1, str2, c, n):
@@ -35,19 +60,37 @@ def estimate(str1, str2, c, n):
     return LS(c, n).estimate(str1, str2)
 
 
-def results_over_c_and_n(str1, str2, c1, c2, n1, n2, step_c, step_n):
-    c_and_n = product(xrange(c1, c2, step_c), xrange(n1, n2, step_n))
+def results_over_c_and_n(str1, str2, c1=C1, c2=C2, n1=N1, n2=N2, step_c=STEP_C,
+                         step_n=STEP_N, true_dist=None):
+    if not (n2 > n1 > 0) or not (c2 > c1 > 0):
+        print()
+        raise ValueError("n1,n2 and c1,c2 must be positive with x1<x2" +
+                         "n1=%s,n2=%s : c1=%s,c2=%s" % (n1, n2, c1, c1))
+
     diff = ScoreDistance.difference
+    c_and_n = product(xrange(c1, c2, step_c), xrange(n1, n2, step_n))
 
-    print("calculating distance...")
+    def err(str1, str2, c, n, distance):
+        return diff(estimate(str1, str2, c, n), distance)
 
-    def err(str1, str2, c, n):
-        # print("estimate: N=%s, C=%s" % (n, c))
-        return diff(estimate(str1, str2, c, n), true_huck_sawyer)
-    results = [(err(str1, str2, c, n), c, n) for c, n in c_and_n]
+    print ("estimating over ranges C=%s,%s and N=%s,%s" % (c1, c2, n1, n2))
 
-    return results
+    if true_dist is None:
+        print("Calculating true distance...")
+        true_dist = Absolute()(str1, str2)
 
-print("Calculating...")
-r = results_over_c_and_n(huck, sawyer, c1=100, c2=200,
-                         n1=5, n2=50, step_c=10, step_n=5)
+    results = [(err(str1, str2, c, n, true_dist), c, n) for c, n in c_and_n]
+
+    return sorted(results)
+
+
+huck_sawyer = results_over_c_and_n(huck, sawyer, true_dist=dist_huck_sawyer)
+
+sibbald_longfellow = results_over_c_and_n(
+    sibbald, longfellow, true_dist=dist_sibbald_longfellow)
+
+longfellow_italian = results_over_c_and_n(
+    longfellow, italian, true_dist=dist_longfellow_italian)
+
+huck_longfellow = results_over_c_and_n(
+    huck, longfellow, true_dist=dist_huck_longfellow)
