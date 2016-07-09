8bbee98f6759f47a0dc9a87700e867bc617930ae
Dodge W. Coates <dcoates91@gmail.com>
2016-07-08 21:56:47 -0400

    Clean compression_time_influence_on_estimate

--- demo/compression_time_influence_on_estimation.py
+++ demo/compression_time_influence_on_estimation.py
@@ -3,95 +3,84 @@ import time
 from decimal import *
 
 from levenshtein.utils import stringer
-from levenshtein.leven_squash import LevenSquash
+from levenshtein.leven_squash import SmartLevenSquash
 from levenshtein.score import ScoreDistance
-from levenshtein.compression import StringCompressorBasic
+from levenshtein.compression import Compressor
+from levenshtein.utils.computation import ComputationManager
 
 getcontext().prec = 6
 
 r = stringer.random_string
 
+create_comp = ComputationManager.CREATE_COMPUTATION
+
 
 def demo(compressor=None):
-    ls = LevenSquash(compressor)
+    if compressor is None:
+        compressor = Compressor()
 
+    ls = SmartLevenSquash(compressor)
     BIG_STR_LEN = 100000
 
     print("Random string length: " + str(BIG_STR_LEN))
 
-    start = time.clock()
-    r1 = r(BIG_STR_LEN)
-    r2 = r(BIG_STR_LEN)
-    end = time.clock()
+    r1 = create_comp(r, BIG_STR_LEN)
+    r2 = create_comp(r, BIG_STR_LEN)
 
-    print("Time to generate two random strings: " + str(end - start) + "\n")
+    print("Time to generate two random strings: %ss\n" %
+          (abs(r1.time() - r2.time())))
 
-    compression_factor = ls.get_compressor().getC()
-    print("Compression factor: " + str(compression_factor) + "\n")
+    compression_factor = ls.getC()
+    print("Compression factor: %s\n" % (compression_factor))
 
-    start = time.clock()
-    sig1 = ls.compress(r1)
-    end = time.clock()
-    t_sig1 = end - start
+    expected_sig_len = BIG_STR_LEN / compression_factor
 
-    print("Time to compress random string: " + str(t_sig1))
+    def compress_(string):
+        sig = ls.compress(string)
 
-    sig1_len = len(sig1)
-    expected_len = BIG_STR_LEN / compression_factor
+        print("Time to compress random string: %ss" % (sig.time()))
 
-    print("Compressed string length: " + str(sig1_len))
-    print("Expected Compressed string length: " + str(expected_len))
-    print("Difference: " + str(ScoreDistance.difference(sig1_len,
-                                                        expected_len)) +
-          "\n")
+        sig_len = len(sig.value())
 
-    start = time.clock()
-    sig2 = ls.compress(r2)
-    end = time.clock()
-    t_sig2 = end - start
+        print("Compressed string length: %s" % (len(sig.value())))
+        print("Expected Compressed string length: %s" % (expected_sig_len))
+        print("Difference: %s\n" % (ScoreDistance.difference(sig_len,
+                                                             expected_sig_len)))
 
-    print("Time to compress random string: " + str(t_sig2))
+        return sig
 
-    sig2_len = len(sig2)
-    expected_len = BIG_STR_LEN / compression_factor
+    sig1 = compress_(r1.value())
+    sig2 = compress_(r2.value())
 
-    print("Compressed string length: " + str(sig2_len))
-    print("Expected Compressed string length: " + str(expected_len))
-    print("Difference: " + str(ScoreDistance.difference(sig2_len,
-                                                        expected_len)) +
-          "\n")
-
-    t_sig_total = t_sig1 + t_sig2
+    t_sig_total = sig1.time() + sig2.time()
     print("Total time to compress strings into signatures: " +
           str(t_sig_total) + "\n")
 
-    start = time.clock()
-    calc = ls.calculate(r1, r2)
-    end = time.clock()
-    t_calc = end - start
+    print("Calculating distance between strings length %s, %s" %
+          (len(r1.value()), len(r2.value())))
 
-    print("Calculated distance: " + str(calc))
-    print("Time to calculate LD: " + str(t_calc) + "\n")
+    calc = ls.calculate(r1.value(), r2.value())
 
-    start = time.clock()
-    sig_dist = ls.calculate(sig1, sig2)
-    end = time.clock()
-    t_est = end - start
-    t_est_total = t_est + t_sig_total
-    est = sig_dist * compression_factor
+    print("Calculated distance: %s" % (calc.value()))
+    print("Time to calculate LD: %ss\n" % (calc.time()))
 
-    print("Estimated distance: " + str(est))
+    sig_dist = ls.calculate(sig1.value(), sig2.value())
+    t_est_total = sig_dist.time() + t_sig_total
+    est = create_comp(lambda: sig_dist.value() * compression_factor)
+    t_est = sig_dist.time() + est.time()
+
+    print("Estimated distance: " + str(est.value()))
     print("Time to estimate LD (not including compression): " + str(t_est))
     print("Time to estimate LD (total time): " + str(t_est_total) + "\n")
 
-    est_err = ScoreDistance.error(est, calc)
+    est_err = ScoreDistance.difference(est.value(), calc.value())
     print("Estimate error: " + "{:.3f}%".format(est_err * 100))
 
     print("Speedup (not including compression time): " +
-          str(int(t_calc / t_est)) +
+          str(int(calc.time() / t_est)) +
           "x")
     print("Speedup (including compression time): " +
-          str(int(t_calc / t_est_total)) +
+          str(int(calc.time() / t_est_total)) +
           "x")
 
     compression_time_factor = t_sig_total / t_est_total
@@ -99,11 +88,14 @@ def demo(compressor=None):
           str(Decimal(compression_time_factor) * Decimal(100)) + "%")
 
 
-def time_compression(sc=StringCompressorBasic()):
+def time_compression(comp=None):
     BIG_STR_LEN = 10000
     t_total = 0
     num_iters = 100
     len_sum = 0
+
+    if comp is None:
+        comp = Compressor()
 
     print("Compressing with " + sc.compress.__name__ + "...")
     print("Compression factor C: " + str(sc.getC()))
