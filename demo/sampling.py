274857b7262b6b3b6b260005afe421c3c9a49aaf
Dodge W. Coates <dcoates91@gmail.com>
2016-07-10 01:01:34 -0400

    Add computation updates, computation results

--- demo/sampling.py
+++ demo/sampling.py
@@ -2,6 +2,7 @@ from itertools import combinations, islice
 from os import listdir
 from os.path import isfile, join
 import pprint
+import time
 
 from levenshtein.utils.stringer import random_string as r
 from levenshtein.distance import Absolute
@@ -28,8 +29,14 @@ def average(samples):
 
 def average_random(length, num):
     s = 0
+    count = 0
+    start = time.clock()
     for i in xrange(num):
+        count += 1
+        print("Progress: %s/%s" % (count, num))
         s += distance(r(length), r(length))
+
+    print("Time to compute: %s" % (time.clock() - start))
 
     return s / float(num)
 
@@ -38,23 +45,31 @@ def average_file(directory, length, num):
     files = [join(directory, f)
              for f in listdir(directory) if isfile(join(directory, f))]
 
-    # make sure there are enough text length-length chunks to make
-    # num combinations of them.
+    # make sure there are enough text chunks to make num combinations
+    # of them.
     txt = ""
+    count = 0
     for f in files:
         print("reading %s..." % (f))
         txt += read(f)
         num_chunks = len(txt) / length
+        count = count + 1
         if num < nCr(num_chunks, 2):
             break
+
+    print("Read %s/%s files in '%s'" % (count, len(files), directory))
 
     chunks = [txt[x:x + length] for x in range(0, len(txt), length)]
 
     print("Calculating distance average of %s measurements of text " +
-          "strings length %s..." %
-          (num, length))
+          "strings length %s...") % (num, length)
     sources = list(islice(combinations(chunks, 2), 0, num))
 
-    s = sum([distance(x, y) for x, y in sources]) / float(len(sources))
+    dists = []
+    count = 0
+    for x, y in sources:
+        count = count + 1
+        print("Progress: %s/%s" % (count, len(sources)))
+        dists.append(distance(x, y))
 
-    return s / float(length)
+    return sum(dists) / float(len(sources)) / float(length)
