d8fb061a0f56aed41dac4a82a8af247729654441
Yury Sobolev <yury.sobolev@gmail.com>
2016-07-10 15:22:08 -0400

    Add cache for true Levenshtein distance.

---
+++ demo/cache.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import json
+import levenshtein.distance
+
+class TrueDistanceCache:
+    def __init__(self, filename=None, data_dir=None):
+        if data_dir == None:
+            data_dir = os.path.join(os.path.dirname(__file__), "data")
+        if filename == None:
+            filename = os.path.join(data_dir, "results", "cache.json")
+        
+        self.filename = filename
+        self.data_dir = data_dir
+
+        if os.path.isfile(filename):
+            with open(filename) as cache_file:
+                self.cache = json.load(cache_file)
+        else:
+            self.cache = {}
+
+        self.levenshtein = levenshtein.distance.Absolute()
+
+    def serialize(self, filename1, filename2):
+        return "__AND__".join(sorted((filename1, filename2)))
+
+    def distance(self, filename1, filename2):
+        filename1 = os.path.join(self.data_dir, filename1)
+        filename2 = os.path.join(self.data_dir, filename2)
+        
+        with open(filename1) as file1, open(filename2) as file2:
+            return self.levenshtein(file1.read(), file2.read())
+
+    def get(self, filename1, filename2):
+        key = self.serialize(filename1, filename2)
+        if key not in self.cache:
+            self.cache[key] = self.distance(filename1, filename2)
+            self.save()
+        return self.cache[key]
+
+    def __getitem__(self, files):
+        return self.get(*files)
+
+    def save(self, filename=None):
+        if filename == None:
+            filename = self.filename
+        
+        with open(filename, 'w') as cache_file:
+            json.dump(self.cache, cache_file)
+
+cache = TrueDistanceCache()
+self = sys.modules[__name__]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: %s filename1 filename2\n" % sys.argv[0])
+        sys.stderr.flush()
+        sys.exit(1)
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+    print cache.get(filename1, filename2)
+else:
+    sys.modules[__name__] = cache
+
