8888c3171d347d8c8088c02b949ac8680f4a2613
Dodge W. Coates <dcoates91@gmail.com>
2016-07-10 03:53:10 -0400

    Add test for average signature distance

--- demo/sampling.py
+++ demo/sampling.py
@@ -3,10 +3,12 @@ from os import listdir
 from os.path import isfile, join
 import pprint
 import time
+import cPickle
 
 from levenshtein.utils.stringer import random_string as r
 from levenshtein.distance import Absolute
 from levenshtein.utils.misc import nCr
+from levenshtein.compression import Compressor, CRCCompression
 
 
 data_dir = "./data/"
@@ -41,7 +43,7 @@ def average_random(length, num):
     return s / float(num)
 
 
-def average_file(directory, length, num):
+def read_files(directory, length, num):
     files = [join(directory, f)
              for f in listdir(directory) if isfile(join(directory, f))]
 
@@ -63,7 +65,12 @@ def average_file(directory, length, num):
 
     print("Calculating distance average of %s measurements of text " +
           "strings length %s...") % (num, length)
-    sources = list(islice(combinations(chunks, 2), 0, num))
+
+    return list(islice(combinations(chunks, 2), 0, num))
+
+
+def average_file(directory, length, num):
+    sources = read_files(directory, length, num)
 
     dists = []
     count = 0
@@ -73,3 +80,29 @@ def average_file(directory, length, num):
         dists.append(distance(x, y))
 
     return sum(dists) / float(len(sources)) / float(length)
+
+
+def average_sig_file(directory, length, num, c, n1, n2, compressor=None):
+    if compressor is None:
+        compressor = Compressor(compression=CRCCompression(), N=10)
+    sources = read_files(directory, length, num)
+
+    count = 0
+    res = []
+    for n in xrange(n1, n2):
+        dists = []
+        compressor.setN(n)
+        count = count + 1
+        print("Progress: %s/%s" % (count, (n2 - n1)))
+        for (x, y) in sources:
+            sigx = compressor.compress(x)
+            sigy = compressor.compress(y)
+            if len(sigx) + len(sigy) != 0:
+                dists.append(distance(sigx, sigy) /
+                             ((len(sigx) + len(sigy)) / 2.0))
+
+        res.append((sum(dists) / float(len(sources)), n, c))
+
+    cPickle.dump(res, open(join(directory, "avg_sig_distance") + '.p', 'wb'))
+
+    return res
--- levenshtein/utils/misc.py
+++ levenshtein/utils/misc.py
@@ -3,4 +3,6 @@ import math
 
 def nCr(n, r):
     f = math.factorial
+    if n < r:
+        return 0
     return f(n) / f(r) / f(n - r)
